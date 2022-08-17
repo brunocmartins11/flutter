1fa6ab6aba034b41e76d1c70a9671698636ce908
Bruno C. Martins <101012137+brunocmartins11@users.noreply.github.com>
2022-08-16 22:01:23 -0300

    Commit 2 - Projeto "Entrada de Pessoas" Completo
    Projeto "Entrada de Pessoas" completo.

--- workspace/app/lib/main.dart
+++ workspace/app/lib/main.dart
@@ -3,18 +3,29 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Batata());
+  runApp(Home());
 }
 
-class Batata extends StatelessWidget {
-  const Batata({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int count = 0;
 
   void decrement() {
-    print("decrement");
+    setState(() {
+      count--;
+    });
   }
 
   void increment() {
-    print("increment");
+    setState(() {
+      count++;
+    });
   }
 
   @override
@@ -22,46 +33,53 @@ class Batata extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             backgroundColor: Colors.red,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Pode entrar",
-                    style: TextStyle(fontSize: 30, color: Colors.white)),
-                Text("0", style: TextStyle(fontSize: 60, color: Colors.white)),
-                Row(
+            body: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/restaurant.jpg"),
+                      fit: BoxFit.cover),
+                ),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextButton(
-                        onPressed: decrement,
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            fixedSize: Size(120, 50),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24))),
-                        child: Text("Saiu",
-                            style:
-                                TextStyle(fontSize: 16, color: Colors.black))),
-                    const SizedBox(
-                      width: 60,
+                    const Text("Pode entrar",
+                        style: TextStyle(fontSize: 30, color: Colors.white)),
+                    Text(count.toString(),
+                        style: TextStyle(fontSize: 60, color: Colors.white)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                            onPressed: decrement,
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                fixedSize: Size(120, 50),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(24))),
+                            child: Text("Saiu",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.black))),
+                        const SizedBox(
+                          width: 60,
+                        ),
+                        TextButton(
+                            onPressed: increment,
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                fixedSize: Size(120, 50),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(24))),
+                            child: Text(
+                              "Entrou",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.black),
+                            ))
+                      ],
                     ),
-                    TextButton(
-                        onPressed: increment,
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            fixedSize: Size(120, 50),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24))),
-                        child: Text(
-                          "Entrou",
-                          style: TextStyle(fontSize: 16, color: Colors.black),
-                        ))
                   ],
-                ),
-              ],
-            )));
+                ))));
   }
 }
-
 /*home: Scaffold(
             appBar: AppBar(
       title: Text("Aula App 1"),
